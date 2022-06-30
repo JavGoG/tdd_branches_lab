d23587e4a71f7b3ae7bba1170ca0876757bc170c
JavGog <javierggjavier@gmail.com>
2022-06-30 13:23:30 -0700

    Football results complete

--- football_results/src/football_results.py
+++ football_results/src/football_results.py
@@ -1,5 +1,10 @@
 def get_result(final_score):
-    pass
+    if final_score["home_score"] < final_score["away_score"]:
+        return "Away win"
+    if final_score["home_score"]   > final_score["away_score"]:
+        return "Home win"
+    if final_score["home_score"] == final_score["away_score"]:
+        return "Draw"
 
 def get_results(final_scores):
     pass
--- football_results/tests/football_results_test.py
+++ football_results/tests/football_results_test.py
@@ -2,7 +2,29 @@ import unittest
 from src.football_results import *
 
 class FootballResultsTest(unittest.TestCase):
-    pass
+    
+    def setUp(self):
+        self.match_1 = {
+            "home_score": 0,
+            "away_score": 1
+        }
+        self.second_match = {
+            "home_score":1,
+            "away_score":0
+        }
+        self.match_3 = {
+            "home_score": 1,
+            "away_score": 1
+        }
+
+    def test_get_result_0_1_return_away_win(self):
+        self.assertEqual("Away win", get_result(self.match_1))
+
+    def test_get_result_1_0_return_home_win(self):
+        self.assertEqual("Home win",get_result(self.second_match) )
+
+    def test_get_result_1_1_return_draw(self):
+        self.assertEqual("Draw", get_result(self.match_3))
     # Test we get the right result string for a final score dictionary representing -
 
         # Home win
@@ -14,3 +36,5 @@ class FootballResultsTest(unittest.TestCase):
 
 if __name__ == "__main__":
     unittest.main()
+
+

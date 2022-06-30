cebf76cd7888005bf2a04aead8b5290e9c775d75
JavGog <javierggjavier@gmail.com>
2022-06-30 14:30:52 -0700

    finish high_scores going to merge

--- high_scores/src/high_scores.py
+++ high_scores/src/high_scores.py
@@ -1,10 +1,23 @@
 def latest(scores):
-    pass
+    return scores[-1]
 
 
 def personal_best(scores):
-    pass
+    sorted_scores = sorted(scores)
+    return sorted_scores[-1]
 
 
 def personal_top_three(scores):
-    pass
+    sorted_scores = sorted(scores, reverse=True)
+    return sorted_scores[0:3]
+
+def ordered_highest(scores):
+    sorted_scores = sorted(scores, reverse=True)
+    last_score = 100 #Highest possible score is 99
+    for score in sorted_scores:
+        if score < last_score:
+            last_score = score
+            pass
+        else:
+            return False
+    return True
--- high_scores/tests/high_scores_test.py
+++ high_scores/tests/high_scores_test.py
@@ -1,25 +1,38 @@
 import unittest
 
-from src.high_scores import latest, personal_best, personal_top_three
+from src.high_scores import latest, ordered_highest, personal_best, personal_top_three
 
 # Tests adapted from `problem-specifications//canonical-data.json` @ v4.0.0
 
 
 class HighScoresTest(unittest.TestCase):
-    
+    def setUp(self):
+        self.list= [22, 34, 11, 6, 78]
+        self.list2 = sorted(self.list, reverse=True)
+        self.list3 = [17, 33, 24, 4, 26, 24]
+        self.list4 = [12 ,16]
+        self.list5 = [1]
     # Tests
 
     # Test latest score (the last thing in the list)
-
+    def test_latest_score(self):
+        self.assertEqual(78, latest(self.list))
     # Test personal best (the highest score in the list)
-
+    def test_personal_best(self):
+        self.assertEqual(78,personal_best(self.list))
     # Test top three from list of scores
+    def test_top_three_from_list(self):
+        self.assertEqual([78, 34, 22],personal_top_three(self.list) )
 
     # Test ordered from highest tp lowest
-
+    def test_ordered_from_highest_to_lowest(self):
+        self.assertEqual(True, ordered_highest(self.list))
     # Test top three when there is a tie
-
+    def test_top_three_if_tie(self):
+        self.assertEqual([33, 26, 24], personal_top_three(self.list3))
     # Test top three when there are less than three
-
+    def test_top_three_if_less_than_three(self):
+        self.assertEqual([16, 12], personal_top_three(self.list4))
     # Test top three when there is only one
-    
+    def test_top_three_when_there_is_only_one(self):
+        self.assertEqual([1], personal_top_three(self.list5))
